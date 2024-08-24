f4bb91b0f4a5b19d38811bf1997427afd9cf3cbd
rishi <rishikr1702@gmail.com>
2024-08-24 13:15:04 +0530

    added comment

--- src/test/java/com/api/automation/GET/getRunner.java
+++ src/test/java/com/api/automation/GET/getRunner.java
@@ -4,9 +4,10 @@ import com.intuit.karate.junit5.Karate;
 
 class getRunner {
 
+
     @Karate.Test
     Karate testUsers() {
         return Karate.run("getEmployees").relativeTo(getClass());
     }
-
+//    just adding a change to try Git Commit
 }
